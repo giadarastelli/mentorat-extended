29d2ef9de0aa76d0b362204ba79abfa20b592fc3
Giada Rastelli <giada.rastelli@stud.hslu>
2020-11-13 14:01:46 +0100

    background color
    background color at function setup

--- mandala-circle/sketch.js
+++ mandala-circle/sketch.js
@@ -5,24 +5,24 @@ function setup() {
 }
 
 function draw() {
-  background(10,10,10,20);
+  background(10, 10, 10, 20);
   noFill();
-  stroke(240,240,240,50);
-  strokeWeight(2); 
-  translate(width/2,height/2); 
-  drawCircle (0,0,600);
+  stroke(240, 240, 240, 50);
+  strokeWeight(2);
+  translate(width / 2, height / 2);
+  drawCircle(0, 0, 600);
 }
 
-function drawCircle(x,y,d) {
-  ellipse (x,y,d);
+function drawCircle(x, y, d) {
+  ellipse(x, y, d);
   if (d > 5) {
-    push(); 
-    translate(0,0); 
-    rotate(-PI /30*frameCount/5);
-    drawCircle (x-d*0.14, y-d*0.14, d*0.6);
-    drawCircle (x, y, d*0.3);
+    push();
+    translate(0, 0);
+    rotate(-PI / 30 * frameCount / 5);
+    drawCircle(x - d * 0.14, y - d * 0.14, d * 0.6);
+    drawCircle(x, y, d * 0.3);
     //drawCircle (x, y+d*0.5, d*0.5);
     //drawCircle (x, y-d*0.5, d*0.5);
-    pop(); 
-    }
+    pop();
+  }
 }
--- spiral-shell/sketch.js
+++ spiral-shell/sketch.js
@@ -1,37 +1,43 @@
 var serieFibonacci = [];
-var a; 
+var i;
 
-function setup(){
-  createCanvas(600, 600);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
   frameRate(30);
-  //noLoop(); 
+  //noLoop();
 }
 
-function draw(){ 
-  background(0,0,0,40);
-  strokeWeight(3);
-  noFill(); 
-  translate(width/3, height/3);
-  rotate(-frameCount /10);
-  for( var n = 0; n < 15; n++) {
-    if(n<=1){
-      a=1; 
+function draw() {
+  background(0, 0, 0, 40);
+  stroke(50, 150, 100);
+  strokeWeight(2);
+  noFill();
+  translate(width / 3, height / 3);
+  rotate(-frameCount / 10);
+
+  for (var n = 0; n < 15; n++) {
+    if (n <= 1) {
+      i = 1;
     } else {
-      a = serieFibonacci[n-1] + serieFibonacci[n-2]; 
+      i = serieFibonacci[n - 1] + serieFibonacci[n - 2];
     }
     //serieFibonacci.push(a); 
-  
+
+    //EXPLAINATION
     //0=>1,1=>1,2=>2,3=>3,4=>5,5=>8 
     //left : key  
     //right: value 
-    
-    serieFibonacci[n]=a; 
-    
-    stroke(255,0,0);
-    //ellipse(0,0,a * 2,a*1.75);
-    arc(0,0,a*2,a*2,0,PI / 2);
-    line(0,a,a*3,a*3); 
-    rotate(PI/2);
-    translate(-serieFibonacci[n-1],0); 
-  } 
+
+    serieFibonacci[n] = i;
+
+    ellipse(0, 0, i * 2, i * 1.75);
+
+    //OTHER FORMS
+    //arc(0, 0, i * 2, i * 2, 0, PI / 2);
+    //line(0, i, i * 3, i * 3);
+    //rect(0, 0, i, i);
+    rotate(PI / 2);
+    translate(-serieFibonacci[n - 1], 0);
+  }
 }
